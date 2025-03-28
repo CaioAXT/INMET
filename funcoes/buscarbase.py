0557162e00199e16376ef79ee5deab329ba59e7d
Caio Rodrigues <caio.alves@araxaeng.com.br>
2025-03-28 18:14:51 -0300

    Update buscarbase.py

--- funcoes/buscarbase.py
+++ funcoes/buscarbase.py
@@ -5,12 +5,11 @@ import pandas as pd
 
 load_dotenv()
 
+ORACLE_PASSWORD = "Eg#Arx_2011$E" 
+ORACLE_DSN = "10.15.1.95/dbprod"
+ORACLE_USER = "ENGENHARIA"
 
 def BuscarBaseINMETporEstacoes(listaestacoes: list):
-
-    ORACLE_USER = os.getenv("ORACLE_USER")
-    ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
-    ORACLE_DSN = os.getenv("ORACLE_DSN")
 
     connection = oracledb.connect(
         user=ORACLE_USER, password=ORACLE_PASSWORD, dsn=ORACLE_DSN
