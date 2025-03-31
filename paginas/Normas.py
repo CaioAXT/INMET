1a016d3ff8b668e687247609d093764ff17e41ed
Caio Rodrigues <caio.alves@araxaeng.com.br>
2025-03-31 17:49:30 -0300

    vai nessa papai kkkkkkkkkk

---
+++ paginas/Normas.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+with st.expander("NBR"):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text("NBR 17.302-2000")
+    with col2:
+        st.link_button(
+            label="XXVIII-SNPTEE-TEMAS-PREFERENCIAIS",
+            url="https://araxaenergiasolar-my.sharepoint.com/:b:/g/personal/servidor_y_araxaeng_com_br/EcTJuVr0T6tGreetfnwvUxkBAsK5C5q9djveh6aThsNa4g?e=iisl0R",
+        )
+with st.container(border=True):
+    st.text("çljkenjgçlojkntrgçljkertgçjknertg")
